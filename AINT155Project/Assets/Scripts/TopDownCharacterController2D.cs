a0e5d91e4542b87d54cf955c827d7b249f0e03eb
Owen H <owenhowarth@gmail.com>
2018-10-23 17:26:59 +0100

    first lesson
    did movement and stuff

---
+++ AINT155Project/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D: MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 velocity = new Vector2(x, y).normalized;
+
+        rigidbody2D.velocity = velocity * speed;
+
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
