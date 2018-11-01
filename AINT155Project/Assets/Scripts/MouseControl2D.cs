b867ef14600994a5d14e3ad78fbf16ddc99fbdee
Owen  H  -  Home <owenhowarth@gmail.com>
2018-11-01 19:10:48 +0000

    Improved mouse movemnts + Sound
    Added sound for shooting just to test shooting.
    
    Locked mouse to center of screen and now use mouse to to point in the direction you  want to shoot

---
+++ AINT155Project/Assets/Scripts/MouseControl2D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl2D : MonoBehaviour {
+
+    private float pitch = 0.0F;
+    private float yaw = 0.0F;
+    public Rigidbody2D hero;
+    public float adjustmentAngle = 0.0f;
+    public float smoothing = 5.0f;
+
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update() 
+    {
+       pitch += Input.GetAxis("Mouse Y");
+       yaw += Input.GetAxis("Mouse X");
+       
+
+        Vector3 direction = new Vector3(yaw, pitch, 0);
+       // direction.Normalize();
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+    }
+
+}
